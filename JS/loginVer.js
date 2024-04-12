3415aab2ee31e6aa53c67cf1c95b4c3abb7316a4
mgt12ga <166156480+mgt12ga@users.noreply.github.com>
2024-04-12 14:02:40 +0200

    aaa

--- JS/loginVer.js
+++
@@ -1,6 +0,0 @@
-    const isLoggedIn = sessionStorage.getItem('loggedIn');
-
-    // If not logged in, redirect to the login page
-    if (!isLoggedIn) {
-        window.location.href = "index.html";
-    }
